438ffce2f34074ff6f0cc9e54ce0db4d19db1b06
Xiaodong Li <xiaodoli@microsoft.com>
2020-04-28 16:38:07 -0700

    turn on aad

---
+++ Common/Common.Auth/AadTokenProvider.cs
@@ -0,0 +1,15 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AadTokenProvider.cs" company="Microsoft Corporation">
+//   Copyright (c) 2020 Microsoft Corporation.  All rights reserved.
+// </copyright>
+// <summary>
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Common.Auth
+{
+    public class AadTokenProvider
+    {
+        
+    }
+}
---
+++ Common/Common.KeyVault/HttpsSettings.cs
@@ -0,0 +1,15 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HttpsSettings.cs" company="Microsoft Corporation">
+//   Copyright (c) 2020 Microsoft Corporation.  All rights reserved.
+// </copyright>
+// <summary>
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Common.KeyVault
+{
+    public class HttpsSettings
+    {
+        
+    }
+}
